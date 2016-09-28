21e90e41ab7e0b9b605cc4642d19405f78078954
1000copy <1000copy@gmail.com>
2016-09-28 17:14:10 +0800

    book first SCAN

---
+++ bs.sh
@@ -0,0 +1,20 @@
+pandoc -o bootstrapbook.epub \
+	book/0.perface.md \
+	book/1.intro.md \
+	book/1.1env.md \
+	book/basic/1.div.md\
+	book/basic/2.list.md\
+	book/basic/3.span.md\
+	book/basic/4.img.md \
+	book/basic/5.button.md \
+	book/basic/6.input.md \
+	book/basic/7.form.md \
+	book/adv/0.dropdown.md\
+	book/adv/1.jumbotron.md\
+	book/adv/2.carousel.md\
+	book/adv/3.navbar.md\
+	book/adv/4.popup.md\
+	book/layout/1.layout.md\
+	book/layout/2.layoutWhy12.md\
+	book/2.pack.md
+	
